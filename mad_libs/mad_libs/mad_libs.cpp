f5e32244780016e696e5dbbce4bbcbbe8dcbd5e4
wigllytest <wigllytest@gmail.com>
2020-07-30 08:18:13 +0300

    update IDE to Visual studio

---
+++ mad_libs/mad_libs/mad_libs.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	string color, pluralNoun, celebrity;
+
+	cout << "Enter color: ";
+	cin >> color;
+
+	cout << "Enter plural noun: ";
+	cin >> pluralNoun;
+
+	cout << "Enter celebrity: ";
+	cin >> celebrity;
+
+
+	cout << "Roses are " << color << endl;
+	cout << pluralNoun << " are blue" << endl;
+	cout << "I love " << celebrity << endl;
+}
